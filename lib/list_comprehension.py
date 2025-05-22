7dad8d249f563d6eed741e04e4e325807233563c
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-05-22 19:48:09 +0300

    passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + "!" for sentence in sentence_list]
+    return exclamation_list
